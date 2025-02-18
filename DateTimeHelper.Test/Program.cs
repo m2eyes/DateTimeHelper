7b99974c3ac981bb6e4a54e00aa4c7aaf0a80fb2
Mahmoud Aboelenin <have_enthusiasm@hotmail.com>
2025-02-18 22:49:01 +0200

    Creating a new project called DateTimeHelper with a test project DateTimeHelper.Test

---
+++ DateTimeHelper.Test/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// read dates from text file and calculate the difference between them
+using DateTimeHelper;
+using System;
+using System.IO;
+using System.Globalization;
+
+namespace DateTimeHelper.Test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Start Fetching Data from File!");
+        Console.WriteLine("================================");
+        string[] lines = File.ReadAllLines(@"..\..\..\TestCases.txt");
+        int i = 0;
+        foreach (var line in lines)
+        {
+            i++;
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
+            
+            var dates = line.Split('-');
+            if (dates.Length != 2)
+            {
+                Console.WriteLine("Invalid date format in line number: " + i);
+                continue;
+            }
+            var startDate = DateTime.Parse(dates[0]);
+            var endDate = DateTime.Parse(dates[1]);
+            var dateDiff = startDate.DateDiff(endDate);
+            Console.WriteLine($"Difference between {startDate} and {endDate} is {dateDiff.years} years, {dateDiff.months} months, {dateDiff.days} days, {dateDiff.hours} hours, {dateDiff.minutes} minutes, and {dateDiff.seconds} seconds");
+        }
+    }
+}
+
---
+++ DateTimeHelper/DateTimeHelper.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+
+namespace DateTimeHelper
+{
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// Convert Gregorian date to Hijri date
+        /// </summary>
+        /// <param name="gregorianDate">Gregorian date</param>
+        /// <returns>Hijri date</returns>
+        /// <remarks>
+        /// This method converts Gregorian date to Hijri date
+        /// </remarks>
+        public static DateTime ToHijriDate(this DateTime gregorianDate)
+        {
+            Calendar umAlQura = new UmAlQuraCalendar();
+            var hijriYear = umAlQura.GetYear(gregorianDate);
+            var hijriMonth = umAlQura.GetMonth(gregorianDate);
+            var hijriDay = umAlQura.GetDayOfMonth(gregorianDate);
+
+            return new DateTime(hijriYear, hijriMonth, hijriDay, umAlQura);
+        }
+
+        /// <summary>
+        /// Get the difference between two dates
+        /// </summary>
+        /// <param name="firstDate"></param>
+        /// <param name="secondDate"></param>
+        /// <returns>The Difference in Years, Months, Days, Hours, Minutes, Seconds</returns>
+        /// <remarks>
+        /// This method returns the difference between two dates in years, months, days, hours, minutes, and seconds
+        /// </remarks>
+        public static (int years, int months, int days, int hours, int minutes, int seconds) DateDiff(this DateTime firstDate, DateTime secondDate)
+        {
+            var result = DateTime.Compare(firstDate, secondDate);
+            if (result == 0)
+            {
+                return (0, 0, 0, 0, 0, 0);
+            }
+            else if (result > 0)
+            {
+                return GetDateDifference(secondDate, firstDate);
+            }
+            return GetDateDifference(firstDate, secondDate);
+        }
+
+        private static (int years, int months, int days, int hours, int minutes, int seconds) GetDateDifference(DateTime startDate, DateTime endDate)
+        {
+            int years = 0, months = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
+            while (startDate < endDate)
+            {
+                if (startDate.AddYears(1) <= endDate)
+                {
+                    startDate = startDate.AddYears(1);
+                    years++;
+                }
+                else if (startDate.AddMonths(1) <= endDate)
+                {
+                    startDate = startDate.AddMonths(1);
+                    months++;
+                }
+                else
+                {
+                    days = (endDate - startDate).Days;
+                    startDate = startDate.AddDays(days);
+                    hours = (endDate - startDate).Hours;
+                    startDate = startDate.AddHours(hours);
+                    minutes = (endDate - startDate).Minutes;
+                    startDate = startDate.AddMinutes(minutes);
+                    seconds = (endDate - startDate).Seconds;
+                    startDate = startDate.AddSeconds(seconds);
+                }
+            }
+            return (years, months, days, hours, minutes, seconds);
+        }
+    }
+}
